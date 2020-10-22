3e7f98a4a73d519c91cbed909ea490192faba820
segmentation <mgrill007@aol.com>
2020-10-22 12:53:32 +0200

    Removed distortion-params to fix wrongly projected points

--- src/run_basics/src/Combinator.cpp
+++ src/run_basics/src/Combinator.cpp
@@ -56,15 +56,16 @@ void addSegmToCloud(pcl::PointCloud<velodyne_pointcloud::PointXYZIR>::Ptr cloud,
 		cloud_Vec.push_back(cv::Point3f(-(cloud->points[i].y),-(cloud->points[i].z),cloud->points[i].x));
 	}
 
-	cv::Mat distCoeffs(5,1,cv::DataType<double>::type);
+	/*cv::Mat distCoeffs(5,1,cv::DataType<double>::type);
 	distCoeffs.at<double>(0) = 0.050773; //0.056972;
 	distCoeffs.at<double>(1) = -0.106031; //-0.114493;
 	distCoeffs.at<double>(2) = -0.001663; //-0.001890;
 	distCoeffs.at<double>(3) = 0.000080; //-0.002819;
-	distCoeffs.at<double>(4) = 0;
-
+	distCoeffs.at<double>(4) = 0;*/
+	cv::Mat distCoeffs = (cv::Mat_<double>(5,1) << 0.050773, -0.106031, -0.001663, 0.000080, 0);
+	
 	vector<cv::Point2f> image_points;
-	cv::projectPoints(cloud_Vec, rvec, tvec, CamA, distCoeffs, image_points);
+	cv::projectPoints(cloud_Vec, rvec, tvec, CamA, cv::Mat(), image_points);
 
 	for (std::size_t i = 0; i < cloud->points.size (); ++i)
 	{
