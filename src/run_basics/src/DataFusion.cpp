3de8287eab2de936360e076a31cc3af88bf43354
segmentation <mgrill007@aol.com>
2020-12-14 17:43:50 +0100

    Remove unused code / comments. Changed hardcoded values to dynamic values form param.

--- src/run_basics/src/DataFusion.cpp
+++ src/run_basics/src/DataFusion.cpp
@@ -27,225 +27,182 @@ ros::Publisher segmCloud_pub;
 ImageConstPtr img_segm;
 PointCloud2::ConstPtr pc_preprocessed;
 
+std::vector<float> camera;
+
+bool getInAngle(double coordinate1, double coordinate2, int angleStart, int angleEnd)
+{
+     float anglePoint = atan2(-coordinate2, coordinate1) * 180 / PI;
+
+    //check angle
+    if (anglePoint > angleStart && anglePoint < angleEnd)
+        return true;
+
+    return false;
+}
+
 void addSegmToCloud(pcl::PointCloud<velodyne_pointcloud::PointXYZIR>::Ptr cloud, cv::Mat fullObjects)
 {
-	double fWidth_px = 1157.519519/4; //1139.081372/4;
-	double fHeight_px = 1158.977934/4; //1140.301608/4;
-	double principal_x = 931.165229/4; //932.960706/4;
-	double principal_y = 529.569982/4; //513.207084/4;
+    //#TODO: adapt camera parameters / add camera parameteres to sensor param file, make variables global?
+    double fWidth_px = 1157.519519/4; //1139.081372/4;
+    double fHeight_px = 1158.977934/4; //1140.301608/4;
+    double principal_x = 931.165229/4; //932.960706/4;
+    double principal_y = 529.569982/4; //513.207084/4;
 
-	cv::Mat CamA = (cv::Mat_<double>(3,3) << fWidth_px, 0, principal_x, 0, fHeight_px, principal_y, 0, 0, 1);
+    cv::Mat CamA = (cv::Mat_<double>(3,3) << fWidth_px, 0, principal_x, 0, fHeight_px, principal_y, 0, 0, 1);
+    
+    float sX = -camera[0];
+    float sY = -camera[1];
+    float sZ = -camera[2];
+    
+    float roll = camera[3];
+    float pitch = camera[4];
+    float yaw = camera[5];
 
-	float PCL_X, PCL_Y, PCL_Z, PCL_Roll, PCL_Yaw, PCL_Pitch;
-	ros::param::get("/PCL_X", PCL_X);
-	ros::param::get("/PCL_Y", PCL_Y);
-	ros::param::get("/PCL_Z", PCL_Z);
-	ros::param::get("/PCL_Roll", PCL_Roll);
-	ros::param::get("/PCL_Pitch", PCL_Pitch);
-	ros::param::get("/PCL_Yaw", PCL_Yaw);
-	float sX = -0.95; //Distance Camera System to Top Lidar System! -0.95, 0, 0.53
-	float sY = 0;
-	float sZ = 0.53;
+    cv::Mat rvec_source = (cv::Mat_<double>(3,1) << pitch, yaw, roll);
+    cv::Mat rvec;
+    cv::Rodrigues(rvec_source, rvec);
 
-	cv::Mat rvec = (cv::Mat_<double>(3,3) << cos(PCL_Yaw * PI / 180.0), 0, -(sin(PCL_Yaw * PI / 180.0)), 0, 1, 0, (sin(PCL_Yaw * PI / 180.0)), 0, cos(PCL_Yaw * PI / 180.0));
-	cv::Mat tvec = (cv::Mat_<double>(3,1) << -sY+PCL_Y, -sZ+PCL_Z, sX+PCL_X);
+    cv::Mat tvec = (cv::Mat_<double>(3,1) << -sY, -sZ, sX);
 
-	vector<cv::Point3f> cloud_Vec;
-	for (std::size_t i = 0; i < cloud->points.size (); ++i)
-	{
-		cloud_Vec.push_back(cv::Point3f(-(cloud->points[i].y),-(cloud->points[i].z),cloud->points[i].x));
-	}
+    cv::Mat distCoeffs = (cv::Mat_<double>(5,1) << 0.050773, -0.106031, -0.001663, 0.000080, 0);
+    //#TODO end
 
-	cv::Mat distCoeffs = (cv::Mat_<double>(5,1) << 0.050773, -0.106031, -0.001663, 0.000080, 0);
-	
-	vector<cv::Point2f> image_points;
-	cv::projectPoints(cloud_Vec, rvec, tvec, CamA, distCoeffs, image_points);
+    vector<cv::Point3f> cloud_Vec;
+    for (std::size_t i = 0; i < cloud->points.size (); ++i)
+    {
+        cloud_Vec.push_back(cv::Point3f(-(cloud->points[i].y),-(cloud->points[i].z),cloud->points[i].x));
+    }
+    
+    vector<cv::Point2f> image_points;
+    cv::projectPoints(cloud_Vec, rvec, tvec, CamA, distCoeffs, image_points);
 
-	for (std::size_t i = 0; i < cloud->points.size (); ++i)
-	{
-		int x = (int)round(image_points[i].x);
-		int y = (int)round(image_points[i].y);
+    for (std::size_t i = 0; i < cloud->points.size (); ++i)
+    {
+        int x = (int)round(image_points[i].x);
+        int y = (int)round(image_points[i].y);
+        cloud->points[i].ring = 0;
 
-		//Check if: img-coord. are in image; point is ahead of camera; point is not near projection plane (distortion correction causes wrong projection)
-		if (0>x || 480<=x || 0>y || 270<=y || cloud->points[i].x < -sX || abs(cloud->points[i].x/cloud->points[i].y) <= 1|| abs(cloud->points[i].x/cloud->points[i].z+sZ) <= 1.44)
-		{
-                //keep information from LiDAR_Net
-			cloud->points[i].ring = 0;
-			cloud->points[i].intensity += 12;
-		}
-		else
-		{
-			cv::Vec3b bgr = fullObjects.at<cv::Vec3b>(y, x);
-                int SegNet_class = (int)bgr[1];
-                int LiDAR_class = cloud->points[i].intensity + 12;
-                if ((SegNet_class == 4 || SegNet_class == 5) && LiDAR_class == 14) //both class road
-                {
-        			cloud->points[i].ring = 0;//(int)bgr[0];
-        			cloud->points[i].intensity = 15;
-                }
-                else if (SegNet_class == 4 || SegNet_class == 5) //SegNet = road
-                {
-                    cloud->points[i].ring = 0; //++maxVal; //TODO: unknown object ID
-                    cloud->points[i].intensity = 17;
-                }
-                else if (LiDAR_class == 14) //LiDAR = road
-                {
-                    cloud->points[i].ring = 0; //TODO: unknown object ID
-                    cloud->points[i].intensity = 16;
-                }
-                else
-                {
-                    cloud->points[i].ring = 0;//(int)bgr[0];
-                    cloud->points[i].intensity = SegNet_class;
-                }
-		}
-	}
+        //Check if: img-coord. are in image; point is ahead of camera; point is not near projection plane (distortion correction causes wrong projection)
+        if (0>x || 480<=x || 0>y || 270<=y || cloud->points[i].x < -sX ||
+                !getInAngle(cloud->points[i].x + sX, cloud->points[i].y + sY, -50, 50)||
+                !getInAngle(cloud->points[i].x + sX, cloud->points[i].z + sZ, -30, 30))
+        {
+            //keep information from LiDAR_Net
+            //cloud->points[i].ring = 0;
+            cloud->points[i].intensity += 12;
+        }
+        else
+        {
+            int SegNet_class = fullObjects.at<uchar>(y, x);
+            int LiDAR_class = cloud->points[i].intensity + 12;
+            if ((SegNet_class == 4 || SegNet_class == 5) && LiDAR_class == 14) //both class road
+            {
+                
+                cloud->points[i].intensity = 15;
+            }
+            else if (SegNet_class == 4 || SegNet_class == 5) //SegNet = road
+            {
+                //cloud->points[i].ring = 0;
+                cloud->points[i].intensity = 17;
+            }
+            else if (LiDAR_class == 14) //LiDAR = road
+            {
+                //cloud->points[i].ring = 0;
+                cloud->points[i].intensity = 16;
+            }
+            else
+            {
+                //cloud->points[i].ring = 0;
+                cloud->points[i].intensity = SegNet_class;
+            }
+        }
+    }
 }
 
 void process_topics()
 {
-	if (nullptr == img_segm || nullptr == pc_preprocessed) return;
+    if (nullptr == img_segm || nullptr == pc_preprocessed) return;
 
-	ROS_INFO("DataFusion - Both received, starting processing");
+    ROS_INFO("DataFusion - Both received, starting processing");
 
-	cv::Mat latest_img;
-	try
-	{
-		latest_img = cv_bridge::toCvShare(img_segm, "bgr8")->image;
-		cv::cvtColor(latest_img, latest_img, cv::COLOR_BGR2GRAY);
-		//cv::imshow("input_img", latest_img);
-		//cv::waitKey(0);
-	}
-	catch (cv_bridge::Exception& e)
-	{
-		ROS_ERROR("DataFusion - Could not convert from '%s' to 'bgr8'.", img_segm->encoding.c_str());
-	}
+    cv::Mat latest_img;
+    try
+    {
+        latest_img = cv_bridge::toCvShare(img_segm, "bgr8")->image;
+        cv::cvtColor(latest_img, latest_img, cv::COLOR_BGR2GRAY);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("DataFusion - Could not convert from '%s' to 'bgr8'.", img_segm->encoding.c_str());
+    }
 
-	cv::Mat fullObjects = cv::Mat::zeros(latest_img.rows, latest_img.cols, CV_8UC3);
-	std::vector<cv::Mat> maskClass;
-	for (uint8_t i=0; i<12; i++)
-	{
-		cv::Mat mask = cv::Mat::zeros(latest_img.rows, latest_img.cols, CV_8UC1);
-		cv::Mat i_mask = cv::Mat(latest_img.rows, latest_img.cols, CV_8UC1, cv::Scalar(i));
+    cv::Mat fullObjects = cv::Mat::zeros(latest_img.rows, latest_img.cols, CV_8UC1);
+    std::vector<cv::Mat> maskClass;
+    for (uint8_t i=0; i<12; i++)
+    {
+        cv::Mat mask = cv::Mat::zeros(latest_img.rows, latest_img.cols, CV_8UC1);
+        cv::Mat i_mask = cv::Mat(latest_img.rows, latest_img.cols, CV_8UC1, cv::Scalar(i));
 
-		cv::compare(latest_img, i_mask, mask, cv::CMP_EQ);
+        cv::compare(latest_img, i_mask, mask, cv::CMP_EQ);
 
-		cv::Mat resultMask(mask.rows, mask.cols, CV_8UC3, cv::Scalar(0,i,0));
-		cv::bitwise_and(resultMask, resultMask, fullObjects, mask);
+        cv::Mat resultMask(mask.rows, mask.cols, CV_8UC1, i); //TODO one channel picture is enough
+        cv::bitwise_and(resultMask, resultMask, fullObjects, mask);
+    }
 
-		//maskClass.push_back(mask);
-		//cv::imshow("input_img", latest_img);
-		//cv::imshow("mask"+std::to_string(i), 255*mask);
-		//cv::imshow("result", result);
-		//cv::waitKey(5000);
-	}
+    pcl::PointCloud<velodyne_pointcloud::PointXYZIR>::Ptr cloud(new pcl::PointCloud<velodyne_pointcloud::PointXYZIR>);
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(*pc_preprocessed, pcl_pc2);
+    fromPCLPointCloud2(pcl_pc2, *cloud);
 
-	//cv::waitKey(0);
+    addSegmToCloud(cloud, fullObjects);
 
-     /*
-	std::vector<std::string> classes_strings {"Sky", "Building", "Pole", "Roadmarking", "Road", "Pavement", "Tree", "Sign-Symbol", "Fence", "Vehicle", "Pedestrian", "Bike"};
-	std::vector<uint8_t> relevant_classes {2,7,10,11}; //Pole, Tree, Pedestrian, Bike (NOT car -> on Buga area no cars allowed)
-	double minArea = 90;
-	int objectNum = 1;
-	int classNum = 0;
-	cv::Mat fullObjects = cv::Mat::zeros(latest_img.rows, latest_img.cols, CV_8UC3);
-	for (auto& mask : maskClass)
-	{
-           if (std::find(relevant_classes.begin(), relevant_classes.end(), classNum) != relevant_classes.end())
-		{
-			std::vector<std::vector<cv::Point>> AllContoursInObject;
-			std::vector<cv::Vec4i> HierarchyOfAllContoursInObject;
-			cv::findContours(mask, AllContoursInObject, HierarchyOfAllContoursInObject, cv::RETR_EXTERNAL , CV_CHAIN_APPROX_SIMPLE);
+    sensor_msgs::PointCloud2 out_msg;
+    pcl::toROSMsg(*cloud.get(),out_msg );
 
-			int index=0;
-			for (auto& cont : AllContoursInObject)
-			{
-				double StapleArea = cv::contourArea(cont);
-		
-				// Check min size
-				if (StapleArea > minArea)
-				{
-					cv::Mat resultMask(mask.rows, mask.cols, CV_8UC3, cv::Scalar(objectNum,classNum,0));
-					cv::Mat ObjectMask = cv::Mat::zeros(mask.rows, mask.cols, CV_8UC1);
-					cv::drawContours(ObjectMask, AllContoursInObject, index, cv::Scalar(255, 255, 255), -1, 8, HierarchyOfAllContoursInObject);
+    img_segm = nullptr;
+    pc_preprocessed = nullptr;
 
-					cv::bitwise_and(resultMask, resultMask, fullObjects, ObjectMask);
-
-					//Print found Objects to input_image (DEBUG)
-					cv::Scalar randCol(rand()&255, rand()&255, rand()&255);
-					cv::drawContours(latest_img, AllContoursInObject, index, randCol, 1, 8, HierarchyOfAllContoursInObject);
-					cv::RotatedRect rect;
-					rect = cv::minAreaRect(AllContoursInObject[index]);
-					std::string objText = std::to_string(objectNum) + " -- " + std::to_string(classNum);
-					cv::putText(latest_img, objText, cv::Point(rect.center.x, rect.center.y), cv::FONT_HERSHEY_SIMPLEX, 0.4, randCol, 1);
-					//                    cv::imshow("ObjectMask", ObjectMask);
-					//                    cv::waitKey(0);
-					objectNum++;
-				}
-				index++;
-			}
-		}
-		else
-		{
-			cv::Mat resultMask(mask.rows, mask.cols, CV_8UC3, cv::Scalar(0,classNum,0));
-			cv::bitwise_and(resultMask, resultMask, fullObjects, mask);
-		}
-		classNum++;
-	}*/
-
-	/* Finished with Image Processing, starting transformation to PointCloud */
-
-	pcl::PointCloud<velodyne_pointcloud::PointXYZIR>::Ptr cloud(new pcl::PointCloud<velodyne_pointcloud::PointXYZIR>);
-	pcl::PCLPointCloud2 pcl_pc2;
-	pcl_conversions::toPCL(*pc_preprocessed, pcl_pc2);
-	fromPCLPointCloud2(pcl_pc2, *cloud);
-
-	addSegmToCloud(cloud, fullObjects);
-
-	sensor_msgs::PointCloud2 out_msg;
-	pcl::toROSMsg(*cloud.get(),out_msg );
-
-	img_segm = nullptr;
-	pc_preprocessed = nullptr;
-
-	segmCloud_pub.publish(out_msg);
+    segmCloud_pub.publish(out_msg);
 }
 
 void ImgCallback(const ImageConstPtr& img_segm_in)
 {
-	ROS_INFO("DataFusion - Received new Image in single CB");
+    ROS_INFO("DataFusion - Received new Image in single CB");
 
-	img_segm = img_segm_in;
+    img_segm = img_segm_in;
 
-	process_topics();
+    process_topics();
 }
 
 void PcCallback(const PointCloud2::ConstPtr& pc_in)
 {
-	ROS_INFO("DataFusion - Received new PC in single CB");
+    ROS_INFO("DataFusion - Received new PC in single CB");
 
-	pc_preprocessed = pc_in;
+    pc_preprocessed = pc_in;
 
-	process_topics();
+    process_topics();
 }
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "DataFusion");
+    ros::init(argc, argv, "DataFusion");
 
-	std::string img_segm_topic = "/BugaSegm/img_segm";
-	std::string pc_preprocessed_topic = "/BugaSegm/pc_segm";
+    std::string img_segm_topic = "/BugaSegm/img_segm";
+    std::string pc_preprocessed_topic = "/BugaSegm/pc_segm";
 
 
-	ros::NodeHandle nh;
+    ros::NodeHandle nh;
 
-	segmCloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/BugaSegm/pc_combined", 1000);
+    segmCloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/BugaSegm/pc_combined", 1000);
 
-	//TimeSynchronizer not applicable because this needs more than one message for each topic!
-	ros::Subscriber subImage = nh.subscribe(img_segm_topic, 1000, ImgCallback);
-	ros::Subscriber subPc = nh.subscribe(pc_preprocessed_topic, 1000, PcCallback);
+    //TimeSynchronizer not applicable because this needs more than one message for each topic!
+    ros::Subscriber subImage = nh.subscribe(img_segm_topic, 1000, ImgCallback);
+    ros::Subscriber subPc = nh.subscribe(pc_preprocessed_topic, 1000, PcCallback);
 
-	ros::spin();
+    // load params
+    ros::param::get("/BASE_TO_CAMERA", camera);
 
-	return 0;
+    ros::spin();
+
+    return 0;
 }
